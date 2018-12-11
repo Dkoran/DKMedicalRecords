98414a75509c6a133fbaabd3ad7c1faac822536d
Rafael Wanjiku <phaelgi@gmail.com>
2018-12-11 09:16:11 +0300

    new files

--- app/src/main/java/hospital/com/dkhospitalrecords/models/AppointmentsCls.kt
+++
@@ -1,3 +0,0 @@
-package hospital.com.dkhospitalrecords.models
-
-data class AppointmentsCls(val hospital_name: String,val hospital_location: String,val hospital_date:String,val status:String)
--- app/src/main/java/hospital/com/dkhospitalrecords/models/HistoryCls.kt
+++
@@ -1,3 +0,0 @@
-package hospital.com.dkhospitalrecords.models
-
-data class HistoryCls (val clinic_name: String,val clinic_log: String,val clinic_location: String,val clinic_email:String)
